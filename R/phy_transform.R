975996c55edb714f301f6e8ed2cec88f1654ef03
ubiminor <57659689+g-antonello@users.noreply.github.com>
2022-05-06 11:20:12 +0200

    now allows Rank inverse Normal transformation

--- R/phy_transform.R
+++ R/phy_transform.R
@@ -2,8 +2,8 @@
 #'
 #' Same as microbiome::transform(), but also adding the hyperbolic arcsine
 #' @param physeq a phyloseq object
-#' @param transform all options available in "microbiome::transform()", extending it with the "asinh" option
-#'
+#' @param transform all options available in "microbiome::transform()", extending it with the \code{arcsinh} ("asinh"), \code{geometric mean}("gm_mean") and \code{Rank Inverse Normal transf.}("irn")
+#' @importFrom RNOmni RankNorm
 #' @return
 #' @export
 #'
@@ -22,22 +22,31 @@
 
 phy_transform <- function(physeq, transform){
 
-  # transform otu counts
-  if(transform %in% c("asinh", "gm_mean")){
-    transformed_otu <- apply(microbiome::abundances(physeq), 1, transform)
+  # if transform is not one of my additional transformations:
+  if(!transform %in% c("asinh", "gm_mean", "irn")){
+    physeq_transf <- microbiome::transform(physeq, transform = transform)
+  }else{ # otherwise, if transform is in my transformations...
 
-  #rownames(t(transformed_otu)) <- taxa_names(physeq)
+    if(transform == "irn"){
+      tmp <- microbiome::transform(physeq, transform = "compositional")
+      transformed_otu <- apply(microbiome::abundances(tmp), 1, RankNorm)
+      rm(tmp)
+    }
+
+    if(transform %in% c("asinh", "gm_mean")){
+      transformed_otu <- apply(microbiome::abundances(physeq), 1, transform)
+    }
+
     physeq_transf <- physeq
+    rm(physeq)
+    # substitute otu table with newly transformed one
     if(taxa_are_rows(physeq_transf)){
       physeq_transf@otu_table <- otu_table(t(transformed_otu), taxa_are_rows = TRUE)
     }else{
       physeq_transf@otu_table <- otu_table(transformed_otu, taxa_are_rows = FALSE)
     }
 
-    # substitute otu table with newly transformed one
-  }else{
-    physeq_transf <- microbiome::transform(physeq, transform = transform)
   }
 
   return(physeq_transf)
-}
+  }
